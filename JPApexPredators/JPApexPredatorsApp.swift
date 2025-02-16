e4bcb8635442124a35a3feb1ec4b9c7f6829cfc8
den330 <wo131485508@qq.com>
2025-02-16 14:49:27 -0500

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by yaxin on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
