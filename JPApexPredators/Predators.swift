c00a205003a15228cc74da2dee151c9bb21ddb63
den330 <wo131485508@qq.com>
2025-02-20 22:22:48 -0500

    L6

--- JPApexPredators/Predators.swift
+++ JPApexPredators/Predators.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Predators {
-    
+        
     var apexPredators: [ApexPredator] = []
     
     init() {
@@ -24,6 +24,26 @@ class Predators {
                 apexPredators = try decoder.decode([ApexPredator].self, from: data)
             } catch {
                 print("Error decoding JSON data :\(error)")
+            }
+        }
+    }
+    
+    func search(for searchTerm: String) -> [ApexPredator] {
+        if searchTerm.isEmpty {
+            return apexPredators
+        } else {
+            return apexPredators.filter { predator in
+                predator.name.localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    func sort(by alphabetical: Bool) {
+        apexPredators.sort { predator1, predator2 in
+            if alphabetical {
+                return predator1.name < predator2.name
+            } else {
+                return predator1.id < predator2.id
             }
         }
     }
