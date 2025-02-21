530724de2f2044caf94fbdd3db0dc698f91193b0
den330 <wo131485508@qq.com>
2025-02-20 21:29:22 -0500

    L4

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let predators = Predators()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        List(predators.apexPredators) { predator in
+            HStack {
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 1)
+                VStack(alignment: .leading) {
+                    Text(predator.name)
+                        .fontWeight(.bold)
+                    Text(predator.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(predator.type.background)
+                        .clipShape(.capsule)
+                }
+            }
+        }.preferredColorScheme(.dark)
     }
 }
 
